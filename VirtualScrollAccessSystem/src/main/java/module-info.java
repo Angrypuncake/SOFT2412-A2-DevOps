549bf26fa0945578d3bf2e71cb52c8b027e173f8
Elson Wijaya <ewij2697@uni.sydney.edu.au>
2024-10-07 14:23:42 +1100

    database and usercontroller handleregister

--- VirtualScrollAccessSystem/src/main/java/module-info.java
+++ VirtualScrollAccessSystem/src/main/java/module-info.java
@@ -7,6 +7,7 @@ module vsas {
     requires net.synedra.validatorfx;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens javafx to javafx.fxml;
     exports javafx;
