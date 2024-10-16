693f7eb87a0b78a3f8770482e710fcb8550080a7
Angrypuncake <elvis70046@gmail.com>
2024-10-16 20:46:16 +1100

    Additional Miscellaneous tests

---
+++ VirtualScrollAccessSystem/src/test/java/javafx/model/UserTypeTest.java
@@ -0,0 +1,38 @@
+package javafx.model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTypeTest {
+
+    @Test
+    void testGuestDisplayName() {
+        assertEquals("Guest", UserType.GUEST.getDisplayName());
+    }
+
+    @Test
+    void testNormalDisplayName() {
+        assertEquals("Normal", UserType.NORMAL.getDisplayName());
+    }
+
+    @Test
+    void testAdminDisplayName() {
+        assertEquals("Admin", UserType.ADMIN.getDisplayName());
+    }
+
+    @Test
+    void testValues() {
+        UserType[] expectedValues = {UserType.GUEST, UserType.NORMAL, UserType.ADMIN};
+        assertArrayEquals(expectedValues, UserType.values());
+    }
+
+    @Test
+    void testValueOf() {
+        assertEquals(UserType.GUEST, UserType.valueOf("GUEST"));
+        assertEquals(UserType.NORMAL, UserType.valueOf("NORMAL"));
+        assertEquals(UserType.ADMIN, UserType.valueOf("ADMIN"));
+    }
+}
